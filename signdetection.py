95cc133a440ddfaef1e4f42037f65d7158e492fc
Jtang6460 <42789671+Jtang6460@users.noreply.github.com>
2021-05-03 14:20:50 -0400

    Update signdetection.py

--- signdetection.py
+++ signdetection.py
@@ -84,8 +84,8 @@ shapeDetected = 0
 
   # command to run servos !
 
-Motor.MotorRun(0, 'forward', 50)
-Motor.MotorRun(1, 'forward', 50)
+Motor.MotorRun(0, 'forward', 40)
+Motor.MotorRun(1, 'forward', 40)
 img_counter = 0
 while stopSignDetected == 0 and aprilTagDetected == 0 and shapeDetected == 0:
 
