7933668165850fb5993cf60fee6760e288100403
Jtang6460 <42789671+Jtang6460@users.noreply.github.com>
2021-05-03 22:38:25 -0400

    Update signdetection.py

--- signdetection.py
+++ signdetection.py
@@ -76,9 +76,6 @@ computervision_client = ComputerVisionClient(endpoint,
         CognitiveServicesCredentials(subscription_key))
 
 # testbench
-command4 = 'python clean.py'
-os.system(command4)
-
 Motor = MotorDriver()
 stopSignDetected = 0
 aprilTagDetected = 0
